3dc4e351e45fcd9de458b936d7c45128945d16f6
Wendell Turner <skyking@ilikecarrots.com>
2025-03-23 12:17:06 -0400

    0.3.2

--- ipyopenlayers/_version.py
+++ ipyopenlayers/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) QuantStack.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 0)
+version_info = (0, 3, 2)
 __version__ = ".".join(map(str, version_info))
